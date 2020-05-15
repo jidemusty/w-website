372e1d4067102507c76503123e6d620145f1b3ac
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2020-05-15 18:03:17 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+// define paths
+app.use(express.static(path.join(__dirname, "../public")));
+const views_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+// set up handlebars engine and view location
+app.set("view engine", "hbs");
+app.set("views", views_path);
+hbs.registerPartials(partials_path);
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Mustapha Babajide",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Andrew Mead",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Weather</h1>");
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Page not found.",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoiamlkZW11c3RhcGhhIiwiYSI6ImNrYTd4MHE1NjA5Mmsyd21yOTNqbW1uZmIifQ.besOQY0uW-_ec-_pEWg2Pw";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;

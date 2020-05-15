e277c4d04cb670bb820a54be8a1d186420437c3d
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2020-05-15 18:36:39 -0500

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // define paths
 app.use(express.static(path.join(__dirname, "../public")));
@@ -101,6 +102,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server started on port 3000");
+app.listen(port, () => {
+  console.log("server started on port " + port);
 });
